f6f62c4a0f44aa57e251eae33e376c14e8d32944
kasefuchs <kasefuchs@protonmail.com>
2025-01-19 14:08:55 +0300

    :construction: Integrate Nomad with Vault

--- terraform/vault/providers.tf
+++ terraform/vault/providers.tf
@@ -1,18 +1,18 @@
 provider "vault" {
-  address      = format("%v://%v", var.vault_scheme, var.vault_address)
+  address      = format("https://%v", var.vault_address)
   ca_cert_file = var.vault_ca_file
   token        = var.vault_token
 }
 
 provider "consul" {
   address = var.consul_address
-  scheme  = var.consul_scheme
+  scheme  = "https"
   ca_file = var.consul_ca_file
   token   = var.consul_token
 }
 
 provider "nomad" {
-  address   = format("%v://%v", var.nomad_scheme, var.nomad_address)
+  address   = format("https://%v", var.nomad_address)
   ca_file   = var.nomad_ca_file
   secret_id = var.nomad_token
 }
