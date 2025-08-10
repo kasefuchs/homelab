3bd6b86c53ce7d747ab9f25e2ab5cf2d58e14922
kasefuchs <kasefuchs@protonmail.com>
2025-08-10 22:53:40 +0300

    :construction: Add Vault Agent to Vault role

--- terraform/vault/resources.tf
+++ terraform/vault/resources.tf
@@ -2,7 +2,7 @@ resource "vault_mount" "pki_root" {
   type                  = "pki"
   path                  = "pki-root"
   description           = "Root PKI"
-  max_lease_ttl_seconds = provider::time::duration_parse("87600h").seconds
+  max_lease_ttl_seconds = local.root_certificate_ttl
 }
 
 resource "vault_pki_secret_backend_config_urls" "pki_root" {
@@ -15,7 +15,7 @@ resource "vault_pki_secret_backend_root_cert" "root" {
   backend     = vault_mount.pki_root.path
   type        = "internal"
   common_name = "Vault PKI Root CA"
-  ttl         = provider::time::duration_parse("87600h").seconds
+  ttl         = local.root_certificate_ttl
 }
 
 resource "vault_mount" "pki_intermediate" {
@@ -39,4 +39,24 @@ resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
 resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
   backend     = vault_mount.pki_intermediate.path
   certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
+}
+
+resource "vault_policy" "vault_agent" {
+  name   = "vault-agent"
+  policy = templatefile("${path.module}/policies/vault-agent.hcl.tftpl", {})
+}
+
+resource "vault_auth_backend" "approle" {
+  type = "approle"
+  path = "approle"
+}
+
+resource "vault_approle_auth_backend_role" "vault_agent" {
+  backend   = vault_auth_backend.approle.path
+  role_name = "vault-agent"
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "vault_agent" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.vault_agent.role_name
 }
