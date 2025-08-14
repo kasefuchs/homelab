c1965fa6b72482ff498b2874ff6e50206aa8e8cf
kasefuchs <kasefuchs@protonmail.com>
2025-08-14 10:14:15 +0300

    :construction: Improve terraform project

--- terraform/vault/locals.tf
+++ terraform/vault/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  root_certificate_ttl = provider::time::duration_parse("87600h").seconds # 10 years.
+  root_certificate_ttl         = provider::time::duration_parse("87600h").seconds # 10 years.
+  intermediate_certificate_ttl = provider::time::duration_parse("43800h").seconds # 5 years.
 }
