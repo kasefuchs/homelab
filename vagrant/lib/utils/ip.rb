70a994ce11acc4558ea29cfd4b56e9483f4c0c1b
kasefuchs <kasefuchs@protonmail.com>
2025-08-18 21:43:31 +0300

    refactor: refactor vagrant docker networking (again)

--- vagrant/lib/utils/ip.rb
+++ vagrant/lib/utils/ip.rb
@@ -12,4 +12,10 @@ module IPUtils
     octets = ip.split(".").map(&:to_i)
     (octets[0] << 24) | (octets[1] << 16) | (octets[2] << 8) | octets[3]
   end
+
+  def self.increment_ip(base_ip, increment)
+    base_int = ip_to_int(base_ip)
+    new_int = base_int + increment
+    int_to_ip(new_int)
+  end
 end
