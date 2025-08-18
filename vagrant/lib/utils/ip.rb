24909e037634b3e94346a02c60369692bf065a16
kasefuchs <kasefuchs@protonmail.com>
2025-08-18 20:39:30 +0300

    refactor: refactor vagrant project

---
+++ vagrant/lib/utils/ip.rb
@@ -0,0 +1,15 @@
+module IPUtils
+  def self.int_to_ip(num)
+    [
+      (num >> 24) & 0xFF,
+      (num >> 16) & 0xFF,
+      (num >> 8) & 0xFF,
+      num & 0xFF
+    ].join(".")
+  end
+
+  def self.ip_to_int(ip)
+    octets = ip.split(".").map(&:to_i)
+    (octets[0] << 24) | (octets[1] << 16) | (octets[2] << 8) | octets[3]
+  end
+end
