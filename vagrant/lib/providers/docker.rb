3a4539febcec5873464104fe4c1f03a6c99bdd2f
kasefuchs <kasefuchs@protonmail.com>
2025-08-18 21:43:31 +0300

    :recycle: Refactor Vagrant Docker networking (again)

--- vagrant/lib/providers/docker.rb
+++ vagrant/lib/providers/docker.rb
@@ -3,18 +3,19 @@ require_relative "../utils/ip"
 module Provider
   def self.configure(node, node_index, node_name, options)
     node.vm.provider :docker do |domain|
-      name_format = options.dig("domain", "name_format")
+      name_format  = options.dig("domain", "name_format")
+      network_args = docker_network_args(node_index, options.dig("network", "private"))
 
       domain.name        = format(name_format, node_name)
       domain.image       = options.fetch("image")
       domain.volumes     = options.dig("storage", "volumes")
-      domain.create_args = options.dig("domain", "create_arguments")
+      domain.create_args = options.dig("domain", "create_arguments") + network_args
     end
+  end
 
-    (options.dig("network", "private") || {}).each do |network_name, network_options|
-      raw_node_ip = IPUtils.ip_to_int(network_options.fetch("start_ip")) + node_index
-
-      node.vm.network :private_network, name: network_name, ip: IPUtils.int_to_ip(raw_node_ip)
-    end
+  def self.docker_network_args(node_index, options)
+    args = ["--network=#{options.fetch('name')}"]
+    args << "--ip=#{IPUtils.increment_ip(options.fetch('start_ip'), node_index)}"
+    args
   end
 end
