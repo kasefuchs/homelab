b5388c40a33dcaeca9ea3237c0a5f94f01085f59
kasefuchs <kasefuchs@protonmail.com>
2025-08-20 10:56:28 +0300

    chore: integrate nomad with consul

--- terraform/consul/outputs.tf
+++ terraform/consul/outputs.tf
@@ -1,9 +1,19 @@
-output "consul_acl_agent_token_secret_id" {
+output "consul_agent_acl_token_secret_id" {
   value     = data.consul_acl_token_secret_id.consul_agent.secret_id
   sensitive = true
 }
 
-output "consul_acl_dns_token_secret_id" {
+output "consul_dns_acl_token_secret_id" {
   value     = data.consul_acl_token_secret_id.consul_dns.secret_id
+  sensitive = true
+}
+
+output "nomad_client_acl_token_secret_id" {
+  value     = data.consul_acl_token_secret_id.nomad_client.secret_id
+  sensitive = true
+}
+
+output "nomad_server_acl_token_secret_id" {
+  value     = data.consul_acl_token_secret_id.nomad_server.secret_id
   sensitive = true
 }
