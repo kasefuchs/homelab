ebc0ff58cc907cdd4c0da6a10d54ae58d5b8e3cf
kasefuchs <kasefuchs@protonmail.com>
2025-08-21 16:02:55 +0300

    :construction: Add Consul auth method for Nomad services

---
+++ terraform/nomad/provision/providers.tf
@@ -0,0 +1,8 @@
+# noinspection MissingProperty
+provider "nomad" {}
+
+# noinspection MissingProperty
+provider "vault" {}
+
+# noinspection MissingProperty
+provider "consul" {}
---
+++ terraform/nomad/provision/variables.tf
@@ -0,0 +1,3 @@
+variable "vault_pki_intermediate_ca_certificate" {
+  type = string
+}
