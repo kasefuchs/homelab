d38eec928fa7e7b21bff9a3fec912ab422fe91a4
kasefuchs <kasefuchs@protonmail.com>
2026-04-17 01:08:35 +0300

    chore(kind): remove redundant dns init container

--- kind/docker/dns/rootfs/usr/local/bin/entrypoint.sh
+++ kind/docker/dns/rootfs/usr/local/bin/entrypoint.sh
@@ -5,5 +5,4 @@ if [ ! -f /var/lib/bind/db.homelab.local ]; then
     cp -n /templates/db.homelab.local /var/lib/bind/db.homelab.local
 fi
 
-chown -R 53:53 /var/lib/bind
-chmod -R 775 /var/lib/bind
+named -u bind -f -c /etc/bind/named.conf -L /var/log/bind/default.log
