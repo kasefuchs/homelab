99dae4620a292c7f417dcfbb3bc8536f3a640a1b
kasefuchs <kasefuchs@protonmail.com>
2026-04-30 15:25:52 +0300

    fix(kind): use homelab.internal domain instead

--- kind/docker/dns/rootfs/usr/local/bin/entrypoint.sh
+++ kind/docker/dns/rootfs/usr/local/bin/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
-if [ ! -f /var/lib/bind/db.homelab.local ]; then
-    cp -n /templates/db.homelab.local /var/lib/bind/db.homelab.local
+if [ ! -f /var/lib/bind/db.homelab.internal ]; then
+    cp -n /templates/db.homelab.internal /var/lib/bind/db.homelab.internal
 fi
 
 named -u bind -f -c /etc/bind/named.conf -L /var/log/bind/default.log
